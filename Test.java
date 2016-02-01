bd54b5eb41a2123a0742f085b753adecb8e5e3e6
Sil3ntPr0mpt <gh0stsync.rt@gmail.com>
2016-02-01 21:52:31 +0700

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test{
+	public static void main(String[] args){
+		String name = "ABCDEFGH";
+
+		System.out.println(name.length());     // หาความยาวของข้อความ
+		System.out.println(name.indexOf('B'));   // หา index ของ ข้อความ
+		System.out.println(name.charAt(2));  // หาผลลัพธ์ของ index
+		System.out.println(name.substring(0, 3)); // ตัดข้อความ
+
+
+	}
+}
